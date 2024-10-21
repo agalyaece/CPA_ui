f5cae153f6dd0ff94c58a3fdd94664dd9487ea4e
agalyaece <agalyapytest@gmail.com>
2024-10-21 13:48:36 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@
 export default function Home  () {
   return (
     <div>
-        <h3 {{ textAlign: 'center',color: 'gray' }} >Home</h3>
+        <h3 style={{ textAlign: 'center',color: 'gray' }} >Home</h3>
         
         
     </div>
